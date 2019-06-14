6bb0d8a08f8d7537b23616a56584781545436977
Kyriakos Psarakis <kpsarakis94@gmail.com>
2019-06-14 17:06:13 +0200

    final JSON to XLSX and figure8 dataset export

--- ML_model/utils/json_to_excel.py
+++ ML_model/utils/json_to_excel.py
@@ -2,7 +2,10 @@ import pandas as pd
 
 
 def from_JSON_to_XLSX(input_path, output_path):
-    pd.read_json(input_path, encoding='utf-8').to_excel(output_path, encoding='utf8')
+    writer = pd.ExcelWriter(path=output_path, engine='xlsxwriter', options={'strings_to_numbers': False,
+                                                                            'strings_to_urls': False})
+    pd.read_json(input_path, dtype='str').to_excel(writer, encoding='utf-8', index=False)
+    writer.save()
 
 
-from_JSON_to_XLSX("../data/dataset_human_vaccination.json", "../data/output.xlsx")
+from_JSON_to_XLSX("../data/dataset_human_vaccination_subsample.json", "../data/figure8_dataset.xlsx")

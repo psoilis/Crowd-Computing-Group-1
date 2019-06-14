8f372888eab0ba164b7e6f5870222f6a509f521d
Panagiotis Soilis <pan.soilis@gmail.com>
2019-06-14 18:38:17 +0200

    Subsample 200

--- ML_model/data/tweet_subsampling.py
+++ ML_model/data/tweet_subsampling.py
@@ -3,6 +3,7 @@ import json
 
 def main():
     positive_bias_tweets = 0
+    negative_bias_tweets = 0
     tweet_data = []
     with open('dataset_human_vaccination.json', 'r') as f:
         data = json.load(f)
@@ -10,12 +11,14 @@ def main():
             # Filters 743 tweets. Sub-sampleed 406 to maintain a 50-50 balance
             if '#vaccineswork' in data[i]['tweet_text']:
                 positive_bias_tweets += 1
-                if positive_bias_tweets <= 406:
+                if positive_bias_tweets <= 100:
                     tweet_data.append(data[i])
 
             # Filters 406 tweets
             if 'antivax' in data[i]['tweet_text'] or 'anti-vax' in data[i]['tweet_text']:
-                tweet_data.append(data[i])
+                negative_bias_tweets += 1
+                if negative_bias_tweets <= 100:
+                    tweet_data.append(data[i])
 
     # Writing final subsampled tweets to file
     with open('dataset_human_vaccination_subsample.json', 'w', encoding='utf8') as outputfile:

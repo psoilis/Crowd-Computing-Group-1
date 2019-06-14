fb66eb1e031f6eefbc226bfa684066f740572844
Kyriakos Psarakis <kpsarakis94@gmail.com>
2019-06-14 17:51:22 +0200

    fixes with the encoding

--- ML_model/utils/json_to_excel.py
+++ ML_model/utils/json_to_excel.py
@@ -4,7 +4,7 @@ import pandas as pd
 def from_JSON_to_XLSX(input_path, output_path):
     writer = pd.ExcelWriter(path=output_path, engine='xlsxwriter', options={'strings_to_numbers': False,
                                                                             'strings_to_urls': False})
-    pd.read_json(input_path, dtype='str').to_excel(writer, encoding='utf-8', index=False)
+    pd.read_json(input_path, dtype='str', encoding='utf-8').to_excel(writer, encoding='utf-8', index=False)
     writer.save()
 
 

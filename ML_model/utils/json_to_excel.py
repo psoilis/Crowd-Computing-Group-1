0d7523d865a4453014fd90d890805553a8e5847f
Kyriakos Psarakis <kpsarakis94@gmail.com>
2019-06-14 10:18:16 +0200

    encoding fix

--- ML_model/utils/json_to_excel.py
+++ ML_model/utils/json_to_excel.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def from_JSON_to_XLSX(input_path, output_path):
-    pd.read_json(input_path).to_excel(output_path)
+    pd.read_json(input_path, encoding='utf-8').to_excel(output_path, encoding='utf8')
 
 
 from_JSON_to_XLSX("../data/dataset_human_vaccination.json", "../data/output.xlsx")
